1ff2b129e15183372619b331bf7f8a24314c643c
PatLewis <patlewis@microsoft.com>
2019-01-08 21:00:41 -0500

    Add files via upload

---
+++ XamIntune/XamIntune/ICreateWelcomeLabel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamIntune
+{
+    public interface ICreateWelcomeLabel
+    {
+        string GetFirstName();
+    }
+}
---
+++ XamIntune/XamIntune/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamIntune
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PopulateTheWelcomeDialog();
+        }
+
+        private async void PopulateTheWelcomeDialog()
+        {
+            try
+            {
+                var WelcomeLabel = DependencyService.Get<ICreateWelcomeLabel>();
+                lblUserInfo.Text = WelcomeLabel.GetFirstName();
+            }
+            catch (Exception Except)
+            {
+                await DisplayAlert("Error in PopulateTheWelcomeDialog", Except.Message, "OK");
+            }
+        }
+    }
+}
